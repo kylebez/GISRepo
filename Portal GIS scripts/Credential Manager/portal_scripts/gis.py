2a9e618606edb0a287f915d355ed9b71dcc207b7
Kyle Beswick <kyle.beswick@nngco.com>
2022-02-07 12:59:40 -0600

    Update documentation, remove corp refs, add portal credential manager

---
+++ Portal GIS scripts/Credential Manager/portal_scripts/gis.py
@@ -0,0 +1,115 @@
+from urllib.parse import urlparse
+from arcgis import GIS as Portal
+from <config-lib> import config
+
+#NOTES:
+# 
+# Use profile manager to save persistent portal logins on the server, so not logging every time this class is instantiated?
+##This method can rid us of secrets files
+#
+# Can assign item id to upload, but must not be used, so need to delete the previous item with that id first. How do we determine the id? Configged?
+#
+# Python version: 3.x
+
+
+class GIS(object):
+	"""
+    A class used for a gis object
+
+    ...
+
+    Attributes
+    ----------
+    portal : GIS
+        the current GIS portal connection instance
+    connUser : str
+        the current username for the GIS portal connection
+
+    Methods
+    -------
+    checkProfile(username)
+        Check if a profile already exists for a given username. Returns Boolean.
+    """
+	def __init__(self,nameOrConn="",url=config.getPortalUrl(config.TARGET_ENV), password="", profileName=None): #if url is not passed in, use the one from the environment config
+		"""
+		Parameters
+        ----------
+        nameOrConn : str
+            A username or connection string. Username will be parsed from connection string, if passed
+        url : str
+            The url for the gis portal connection. Default will retreive this from the current environment config file
+        password : str
+            The password for the gis portal connection
+		profileName: str
+			A way to pass in a profile name (to match the config file) - will default to username if nothing passed
+		"""
+		if nameOrConn == "":
+			raise Exception("Missing connection or username parameters")
+
+		try:
+			#automatically parse out username from a valid connection string or file path, path detected if it has the format server@database@user.sde
+			username = nameOrConn.split('@')[1].split('.')[0]
+		except IndexError:
+			#if not an connection string or file path, assume the parameter is passing in a username			
+			username = nameOrConn
+		finally:
+		#check if username exists as a profile, if password is not passed in, see if config can get it
+			if password == "":
+				password = config.getPassword(username)
+			if profileName is None:
+						profileName = username
+			if Portal.checkProfile(profileName):
+				self.portal = Portal(profile=profileName)
+			else:
+				# If profile doesn't exist then make one
+				try:
+					self.portal = Portal(url, username, password, profile=profileName)
+				except Exception:
+					raise Exception("Unable to connect to portal with username '{0}'".format(username))
+			self.connUser = username
+
+	def sharePortalItems(self,name,share_group):
+		if isinstance(share_group, str):
+			share_group = None if share_group == 'None' or share_group == '' else share_group.split(',') 
+		shareArgs = [False,False,None]
+		connUser_content = self.connUser.items()
+		for item in connUser_content:
+			if name == item.title:
+				for group in share_group:
+					if group in ['Everyone', 'Public']:
+						shareArgs[0]=True
+					elif group == "Org":
+						shareArgs[1]=True
+					else:
+						shareArgs[2].append(group) 
+				item.share(*shareArgs)
+
+	def getUsers(self,name): #Pass in None to return all users
+		users = self.portal.users.search(name) #pylint: disable=no-member
+		return users
+
+	def getEnv(self):
+		try:
+			return (urlparse(self.url).netloc).split(".")[0].split('-')[1]
+		except KeyError:
+			return "prod"
+	@classmethod
+	def checkProfile(cls,name):
+		if name in Portal.ProfileManager().list(): #pylint: disable=no-value-for-parameter
+			return True
+		else:
+			return False
+	
+	@classmethod
+	def delProfile(cls,name):
+		Portal.ProfileManager().delete(name)
+
+	@classmethod
+	def updateProfile(cls,name,uname = None,pwd = None):
+		if uname is None:
+			uname = config.getUsername(name)
+		if pwd is None:
+			pwd = config.getPassword(name)
+		Portal.ProfileManager().update(name,username=uname,password=pwd)
+
+
---
+++ Portal GIS scripts/Credential Manager/storeLoginPasswords.py
@@ -0,0 +1,85 @@
+import keyring
+import getpass
+import os
+from <config_lib> import config
+from configparser import ConfigParser
+from portal_scripts.gis import GIS
+
+# Moved all usernames to config.ini, this script takes that and asks for passwords and then puts them into OS cred store
+# Used to instantiate passwords on servers and the like
+#
+# Python version 3.x
+
+noUpdate = False
+def doAllServices():
+	parser = ConfigParser()
+	parser.read(os.path.join(os.path.dirname(config.__file__),"config.ini"))
+	for s in services:
+		setPasswordForService(s)
+	return
+
+def setPasswordForService(s):
+	n = config.getUsername(s)
+	try:
+		if keyring.get_password(s,n) is None:
+			if "gis_portal" in s: #If it has gis_portal in the name, it should be a portal
+				isPortal = True
+				if GIS.checkProfile(s):
+					managePassword(s,n,isPortal)
+					return
+			else:
+				isPortal = False
+			pword = getpass.getpass("\nSet the password for the {0} service with the username {1}: ".format(s,n))
+			if isPortal:
+				print("This is a GIS portal, trying to connect")
+				try:
+					GIS(n, url = config.getPortalUrl(s.split("_")[0]), password = pword, profileName=s)
+					print("Success")
+				except Exception as e:
+					print(e)
+					print("No portal, setting up general credential")
+			else:
+				keyring.set_password(s,n,pword)
+		else:
+			managePassword(s,n)
+	except keyring.errors.PasswordSetError:
+		print("Unable to set password")
+	return
+
+def managePassword(service, name, isPortal = False):
+	print("\n{0} already set with a password".format(service))
+	global noUpdate
+	if noUpdate:
+		return
+	u = input("Next action? (u) for update, (r) for remove, (n) for do nothing, or (x) for do not ask again)").lower()
+	if u == 'x':
+		noUpdate = True
+	if u == 'u':
+		pword = passwordPrompt(service, name)
+		if isPortal:
+				GIS.updateProfile(service,pwd=pword)
+		else:
+			keyring.set_password(service,name,pword)
+	if u == 'r':
+		if isPortal:
+			GIS.delProfile(service)
+		else:
+			config.delPassword(service)
+	if isPortal:
+		pass
+
+def passwordPrompt(service,name):
+	return getpass.getpass("\nSet the password for ({0})/{1}: ".format(service,name))
+
+parser = ConfigParser()
+parser.read(os.path.join(os.path.dirname(config.__file__),"config.ini"))
+services = sorted(dict(parser.items("logins")).keys())
+print("Available services for configuration:")
+for l in services:
+	print(l)
+i = input("\nDefine credentials for what service? (Use 'all' to define for all listed) ").lower()
+if i == 'all':
+	doAllServices()
+else:
+	setPasswordForService(i)
+print("\nPassword set complete")
